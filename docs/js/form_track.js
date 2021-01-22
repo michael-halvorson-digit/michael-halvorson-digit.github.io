ba27821bf1468b6dcb6422d4a1861b22031ffd5f
michael-halvorson-digit <michael.halvorson@digit.co>
2021-01-22 14:20:06 -0800

    added waitlist form tracking
    Also minor modifications to tracking script to use local properties

--- docs/js/form_track.js
+++ docs/js/form_track.js
@@ -73,7 +73,7 @@ window.addEventListener('load', function () {
     window.urlParams = new URLSearchParams(window.location.search);
     window.anonymousId = window.analytics.user().anonymousId();
     window.trackingProperties.anonymousId = window.anonymousId;
-    window.analytics.track("Viewed Invite Landing Page", window.trackingProperties);
+    window.analytics.track("Viewed Waitlist Landing Page", window.trackingProperties);
     //console.log("VILP tracked");
 });
 
@@ -87,6 +87,16 @@ window.addEventListener('DOMContentLoaded', function () {
         var localClickProperties = window.trackingProperties;
         window.analytics.track(window.trackingEvents.CLICKED_APP_STORE_BUTTON, localClickProperties);
     }
+    function submit_handler(event) {
+        var localClickProperties = window.trackingProperties;
+        localClickProperties.formData = serialize(this);
+        window.analytics.track('Submitted Waitlist Form', localClickProperties);
+    }
+    
+    // Add tracking to form links
+    Array.prototype.map.call(document.querySelectorAll('form'), function (a) {
+        return a.addEventListener("submit", submit_handler);
+    });
 
     // Add click tracking to all links that go to digit.co/signup
     Array.prototype.map.call(document.querySelectorAll('[href*="digit.co/signup"]'), function (a) {
@@ -98,3 +108,55 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
 });
+
+var serialize = (function (slice) {
+    return function (form) {
+        //no form, no serialization
+        if (form == null)
+            return null;
+
+        //get the form elements and convert to an array
+        return slice.call(form.elements)
+            .filter(function (element) {
+                //remove disabled elements
+                return !element.disabled;
+            }).filter(function (element) {
+                //remove unchecked checkboxes and radio buttons
+                return !/^input$/i.test(element.tagName) || !/^(?:checkbox|radio)$/i.test(element.type) || element.checked;
+            }).filter(function (element) {
+                //remove <select multiple> elements with no values selected
+                return !/^select$/i.test(element.tagName) || element.selectedOptions.length > 0;
+            }).filter(function (element) {
+                //remove empty name & value elemnts
+                return !(element.name == '' && element.value == '');
+            }).map(function (element) {
+                switch (element.tagName.toLowerCase()) {
+                    case 'checkbox':
+                    case 'radio':
+                        return {
+                            name: element.name,
+                            value: element.value === null ? 'on' : element.value
+                        };
+                    case 'select':
+                        if (element.multiple) {
+                            return {
+                                name: element.name,
+                                value: slice.call(element.selectedOptions)
+                                    .map(function (option) {
+                                        return option.value;
+                                    })
+                            };
+                        }
+                        return {
+                            name: element.name,
+                            value: element.value
+                        };
+                    default:
+                        return {
+                            name: element.name,
+                            value: element.value || ''
+                        };
+                }
+            });
+    }
+}(Array.prototype.slice));
